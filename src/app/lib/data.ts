bc45e3ef0d93e059bffdc2ab517c9842d4830431
Atulbende <107322576+Atulbende@users.noreply.github.com>
2024-08-27 00:04:31 +0530

    career changes

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -25,8 +25,8 @@ export const education=[
             {class:'MSC IT',date:'2016',place:'S.V.S Collage Chhindwara',location:'MakhanLal Chaturvedi Univercity Bhopal(M.P.)',bgColor:'radial-gradient(78.57% 78.57% at 50.65% 0.84%,#e9d5ff 0%,#8383fd 0%,rgba(135, 160, 219, 0) 100%);',Color:'#fff'}
 ];
 export const career=[
-    {company:'Freelance Project',date:'07/2016 - 12/2019',position:'full Stack Developer',place:'I have completed 30+ projects as Freelance Online and Desktop Applications in Chhindwara(M.P.) ',bgColor:'radial-gradient(78.57% 78.57% at 50.65% 0.84%,#e9d5ff 0%,#8383fd 0%,rgba(135, 160, 219, 0) 100%);',Color:'#fff'},
-    {company:'W3bytes',date:'02/12/2019 – 04/07/2023',position:'Consultant',place:'Amar Plaza, 17/1, IT Park Rd, Gayatri Nagar, Pratap Nagar, Nagpur, Maharashtra 440022 (Closed )',bgColor:'radial-gradient(78.57% 78.57% at 50.65% 0.84%,#e9d5ff 0%,#8383fd 0%,rgba(135, 160, 219, 0) 100%);',Color:'#fff'},
+    {company:'Freelance Project',date:'07/2016 - 12/2019',position:'Full Stack Developer',place:'I have completed 30+ projects as Freelance Online and Desktop Applications in Chhindwara(M.P.) ',bgColor:'radial-gradient(78.57% 78.57% at 50.65% 0.84%,#e9d5ff 0%,#8383fd 0%,rgba(135, 160, 219, 0) 100%);',Color:'#fff'},
+    {company:'W3bytes',date:'02/12/2019 – 04/07/2023',position:'Senior Consultant',place:'Amar Plaza, 17/1, IT Park Rd, Gayatri Nagar, Pratap Nagar, Nagpur, Maharashtra 440022 (Closed )',bgColor:'radial-gradient(78.57% 78.57% at 50.65% 0.84%,#e9d5ff 0%,#8383fd 0%,rgba(135, 160, 219, 0) 100%);',Color:'#fff'},
     {company:'Kfintech',date:'10/07/2023 – 02-08-2024',position:'Senior Software Engineer',place:'Selenium Building, Tower – B, Plot No 31 & 32, Financial District Nanakramguda, Serilingampalle (M), Hyderabad, Telangana 500032',bgColor:'radial-gradient(78.57% 78.57% at 50.65% 0.84%,#e9d5ff 0%,#8383fd 0%,rgba(135, 160, 219, 0) 100%);',Color:'#fff'}
       ];
 
